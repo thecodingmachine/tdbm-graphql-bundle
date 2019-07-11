5cf67534e04536f56d7d4a5fb57b7ca50df87d4a
David Négrier <d.negrier@thecodingmachine.com>
2019-07-11 15:46:35 +0200

    Upgrading to tdbm-graphql v4

--- DependencyInjection/TdbmGraphqlExtension.php
+++ DependencyInjection/TdbmGraphqlExtension.php
@@ -8,7 +8,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use TheCodingMachine\Tdbm\GraphQL\GraphQLTypeAnnotator;
 
 class TdbmGraphqlExtension extends Extension
 {
@@ -20,14 +19,7 @@ class TdbmGraphqlExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/container'));
         $loader->load('tdbmgraphql.xml');
-
-        $definition = $container->getDefinition(GraphQLTypeAnnotator::class);
-        $definition->replaceArgument(0, $config['type_namespace']);
-        $definition->replaceArgument(1, $config['generated_type_namespace']);
     }
 }
