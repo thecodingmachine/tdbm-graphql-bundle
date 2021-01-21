62cb0cc5d29d0584a85bd801c56ec2c2f9045533
Guillaume <g.alabre@thecodingmachine.com>
2021-01-21 15:25:33 +0100

    TDBM Bundle automatically register the code generator listeners

--- TheCodingMachineTdbmGraphqlBundle.php
+++ TheCodingMachineTdbmGraphqlBundle.php
@@ -3,17 +3,8 @@
 
 namespace TheCodingMachine\Tdbm\Graphql\Bundle;
 
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use TheCodingMachine\Tdbm\Graphql\Bundle\DependencyInjection\TheCodingMachineTdbmGraphqlCompilerPass;
 
 class TheCodingMachineTdbmGraphqlBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new TheCodingMachineTdbmGraphqlCompilerPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 1);
-    }
 }
